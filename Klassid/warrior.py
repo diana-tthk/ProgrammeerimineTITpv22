8963fa82f045676c77ad9149da32970547207c74
diana-tthk <112874813+diana-tthk@users.noreply.github.com>
2023-04-24 13:30:45 +0300

    tund 24.04 (esimene osa)

---
+++ Klassid/warrior.py
@@ -0,0 +1,55 @@
+#we have class Warrior
+#every warrior has some health (100 p. at the beginning)
+#Warrior may attack. When attacking,
+#the warrior he attack looses health 
+#random number between 1 and 20
+#Write a game, with two warriors attacking in random order
+#After each attack print who attacked whom and how 
+#many health they have left. Once someone has no health
+#left, game is over and the winner is printed
+import random
+class Warrior:
+    name = ""
+    hp = 100
+
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self, warrior):
+        if isinstance(warrior, Warrior):
+            print(self.name, "is attacking", warrior.name)
+            warrior.under_attack()
+        else:
+            print("Cannot attack this object!")
+        return
+    
+    def under_attack(self):
+        self.hp = self.hp - random.randint(1,20)
+        print(self.name, "has", self.hp, "health")
+        return
+
+
+#main logic
+war1 = Warrior("Marek") 
+war2 = Warrior("Peter")
+
+def end_game(w1, w2):
+    if w1.hp <= 0 or w2.hp <= 0:
+        return True
+    else:
+        return False
+
+while True:
+    lot = random.randint(1,2)
+    if lot == 1:
+        war1.attack(war2)
+        if(end_game(war1, war2)):
+            print(war1.name, "has won the game!")
+            break
+    else:
+        war2.attack(war1)
+        if(end_game(war1, war2)):
+            print(war2.name, "has won the game!")
+            break
+
+input()
---
+++ Klassid/warrior_amunition.py
@@ -0,0 +1,8 @@
+# we have warrior (can use the one from warrior.py as a
+# basis). Also we have several weapons: sword, frist
+# Sword makes 10-20 of damage. Frist makes 0-15
+# Warrior can have sword (if no weapon, then 
+# warrior uses only frists). At the begginig of game
+# warriors roll the dice. If ones score is 3 or more,
+# one gets a sword. Extend the game writen previously 
+# according to this scenario
